5ba3ce707fb1076cdc2d4190b9bd9fb18a809552
jaydave2201 <jaydave2203@gmail.com>
2024-07-26 00:25:37 +0530

    car page

---
+++ carrental/app/src/main/java/com/example/carrental/Constant.java
@@ -0,0 +1,14 @@
+package com.example.carrental;
+
+public class Constant {
+
+    public static final String PREF = "pref";
+
+    public static final String NAME = "name";
+    public static final String EMAIL = "email";
+    public static final String CONTACT = "contact";
+    public static final String PASSWORD = "password";
+
+
+
+}
